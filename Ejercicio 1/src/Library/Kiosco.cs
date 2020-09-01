fa41ddd335de99100700bb28d06c0a47899c02ae
gebchuy <gebchuy@gmail.com>
2020-09-01 19:36:38 -0300

    Comentarios sobre patrones

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -4,11 +4,13 @@ namespace Expert_SRP
 {
     public class Kiosco 
     {
+        // No cumple Expert: la clase Kiosco no es experta en dinero
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
             return pesos >= a.PrecioDulce + a.PrecioMasa;
         }
+        // No cumple SRP: deberia ser responsabilidad de una clase ConversionMoneda
         private Double ConvertirAPesos(Double dinero, String moneda)
         {
             if (moneda.Equals("U$S")) 
