99accde2dc2031e7832ab1ffeaf0a4fd3466d874
gebchuy <gebchuy@gmail.com>
2020-09-01 20:38:41 -0300

    Solucion propuesta para el ejercicio 1

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -11,5 +11,10 @@ namespace Expert_SRP
         }
         public Double PrecioDulce {get;set;}
         public Double PrecioMasa {get; set;}
+
+        public Double PrecioFinal()
+        {
+            return this.PrecioDulce + this.PrecioMasa;
+        }
     }
 }
--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -4,27 +4,12 @@ namespace Expert_SRP
 {
     public class Kiosco 
     {
-        // No cumple Expert: la clase Kiosco no es experta en dinero
+        
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
-            Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa;
+            Double pesos = ConversorMoneda.ConvertirAPesos(dinero, moneda);
+            return pesos >= a.PrecioFinal();
         }
-        // No cumple SRP: deberia ser responsabilidad de una clase ConversionMoneda
-        private Double ConvertirAPesos(Double dinero, String moneda)
-        {
-            if (moneda.Equals("U$S")) 
-            {
-                return dinero / 30;
-            } 
-            else if (moneda.Equals("$")) 
-            {
-                return dinero;
-            }
-            else 
-            {
-                return -1;
-            }
-        }
+
     }
 }
